7f3b73c9be50960ec6583cd9f6a0b8af23a2c773
Varad Patil <varadrpatil27@gmail.com>
2021-08-11 08:13:01 +0530

    initialize project structure

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$page_title = "Welcome to XKCD Comic Mailer, Let's Get Started";
+require_once __DIR__ . '\includes\navbar.php';
+?>
+<div class="main-div center-div">
+    <h1>Hello World</h1>
+</div>
+
+<?php
+require_once __DIR__ . '\includes\footer.php';
+?>
