7a37f6f97ab004b8f36b3be5e4b7f8fb8a6c0db0
Varad Patil <varadrpatil27@gmail.com>
2021-08-11 08:51:00 +0530

    phpcs fixes

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $page_title ?></title>
-    <link rel="stylesheet" href="./public/css/styles.css">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
-</head>
+    <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $page_title ?></title>
+	<link rel="stylesheet" href="./public/css/styles.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+    </head>
 
-<body>
+    <body>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-$page_title = "Welcome to XKCD Comic Mailer, Let's Get Started";
-require_once __DIR__ . '\includes\navbar.php';
+	$page_title = "Welcome to XKCD Comic Mailer, Let's Get Started";
+	require_once __DIR__ . '\includes\navbar.php';
 ?>
 <div class="main-div center-div">
-    <h1>Hello World</h1>
+	<h1>Hello World</h1>
 </div>
 
 <?php
-require_once __DIR__ . '\includes\footer.php';
+	require_once __DIR__ . '\includes\footer.php';
 ?>
